e9857691574bf15199785c6057464651e7a0514e
nathanSamosa <nathanthesamosa@gmail.com>
2021-12-02 17:53:33 +0000

    storeItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './styles/reset.css'
 import './styles/index.css'
+import StoreItem from './components/store-item'
+import StoreArr from './data/items'
+import { useState } from 'react'
 
 /*
 Here's what a store item should look like
@@ -15,14 +18,23 @@ What should a cart item look like? 🤔
 const initialStoreItems = []
 
 export default function App() {
-  // Setup state here...
+
+  const addToCart = (e) => {
+    console.log(e)
+  }
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul class="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          {StoreArr.map(grocery =>
+              <StoreItem
+                key={grocery.id}
+                grocery={grocery}
+                addToCart={addToCart}
+              />
+          )}
         </ul>
       </header>
       <main id="cart">
--- src/data/items.js
+++ src/data/items.js
@@ -12,49 +12,49 @@ export default [
         image: "assets/icons/002-carrot.svg"
     },
     {
-        is: "003-apple",
+        id: "003-apple",
         name: "apple",
         price: 0.62,
         image: "assets/icons/003-apple.svg"
     },
     {
-        is: "004-apricot",
+        id: "004-apricot",
         name: "apricot",
         price: 1.75,
         image: "assets/icons/004-apricot.svg"
     },
     {
-        is: "005-avocado",
+        id: "005-avocado",
         name: "avocado",
         price: 0.75,
         image: "assets/icons/005-avocado.svg"
     },
     {
-        is: "006-bananas",
+        id: "006-bananas",
         name: "bananas",
         price: 0.64,
         image: "assets/icons/006-bananas.svg"
     },
     {
-        is: "007-bell-pepper",
+        id: "007-bell-pepper",
         name: "bell-pepper",
         price: 0.45,
         image: "assets/icons/007-bell-pepper.svg"
     },
     {
-        is: "008-berry",
+        id: "008-berry",
         name: "berry",
         price: 2.00,
         image: "assets/icons/008-berry.svg"
     },
     {
-        is: "009-blueberry",
+        id: "009-blueberry",
         name: "blueberry",
         price: 2.00,
         image: "assets/icons/009-blueberry.svg"
     },
     {
-        is: "010-eggplant",
+        id: "010-eggplant",
         name: "eggplant",
         price: 0.69,
         image: "assets/icons/010-eggplant.svg"
